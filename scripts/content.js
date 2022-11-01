0f2256446573b0a0c746d513a070e28e91d0e470
zbw <zbw0517@gmail.com>
2022-10-31 20:02:12 -0700

    format html a bit

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,16 @@
 (async () => {
   const { enabled } = await chrome.storage.local.get("enabled");
   if (enabled) {
+    const html = `
+      <html>
+        <head>
+         <style>.center {text-align: center;font-size:40px;}</style>
+        </head><body>
+        <div class="center"><p>FOCUS!</p></div>
+      </html>
+    `;
     document.open();
-    document.write(
-      '<html><head><style>.center {text-align: center;font-size:40px;}</style></head><body><div class="center"><p>FOCUS!</p></div>'
-    );
+    document.write(html);
     document.close();
   }
 })();
